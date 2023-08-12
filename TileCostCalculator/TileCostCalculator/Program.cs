1c3138f00418f4849711d9fa64e4b22a000686de
Layusmen <67705870+Layusmen@users.noreply.github.com>
2023-08-12 20:19:59 +0100

    Program.cs

--- TileCostCalculator/TileCostCalculator/Program.cs
+++ TileCostCalculator/TileCostCalculator/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace TileCostCalculator
 {
@@ -7,77 +7,75 @@ namespace TileCostCalculator
         static void Main(string[] args)
         {
             // Supply the shape of the room
-            Console.WriteLine("Please state whether the shape of the room is triangular or rectangular");
-            string ShapeofRoom = Console.ReadLine();
+            Console.WriteLine("Please select whether the shape of the room. For rectangular, enter r. For Triangle enter t.");
+            string shapeofRoom = Console.ReadLine();
 
             // Convert ShapeofRoom to Uppercase
-            string NewShape = ShapeofRoom.ToUpper();
+            string newShape = shapeofRoom.ToUpper();
 
             // Supply the Cost/unit of Tile
             Console.WriteLine("Please Supply the Cost of Tile/Square Feet in $ : ");
-            double CostofTile = Convert.ToDouble(Console.ReadLine());
+            double costofTile = Convert.ToDouble(Console.ReadLine());
 
             // Supply the size of each unit of tiles in square feet
             Console.WriteLine("Size of each unit of tiles in square feet : ");
-            double SizeofTile = Convert.ToDouble(Console.ReadLine());
+            double sizeofTile = Convert.ToDouble(Console.ReadLine());
+
+            double tilesNeeded;
+            double area = 0;
+            double total;
+            double labourCost;
 
             // Calculating the Area to floor 
-            if (NewShape == "RECTANGLE")
+            if (newShape == "R")
+
             {
+                //Supply the lenght
                 Console.WriteLine("Good day. Please Supply the flooring Length in feet");
-                double LengthofRoom = Convert.ToDouble(Console.ReadLine());
+                double lengthofRoom = Convert.ToDouble(Console.ReadLine());
 
                 // Supply the Width
                 Console.WriteLine("Please Supply the flooring width in feet");
-                double WidthofRoom = Convert.ToDouble(Console.ReadLine());
+                double widthofRoom = Convert.ToDouble(Console.ReadLine());
 
                 // Calculating the Area to floor 
-                double Area = LengthofRoom * WidthofRoom;
-
-                // Tiles needed 
-                double TilesNeeded = Area / SizeofTile;
-                Console.WriteLine("The total tiles needed is: " + TilesNeeded + ".");
-
-                // Calculating the Total Cost
-                double total = TilesNeeded * CostofTile;
-                Console.WriteLine("The Total Cost is: $ " + total + ".");
-                //since work done on 20 square feet is 
-                //#86/hr. Then, I square feet costs 86/20 = $4.3
-                double labourCost = Area * (86 / 20);
-                Console.WriteLine("The labor cost is: $" + labourCost);
-
+                area = lengthofRoom * widthofRoom;
+                Console.WriteLine("Area of Rectangle is: " + area);
 
             }
-            else if (NewShape == "TRIANGLE")
+            else if (newShape == "T")
             {
+                //Supply the bas
                 Console.WriteLine("Enter the base of triangle:");
-                double TriangleBase = Convert.ToDouble(Console.ReadLine());
+                double triangleBase = Convert.ToDouble(Console.ReadLine());
+
+                //Supply the height
                 Console.WriteLine("Enter the height of triangle:");
-                double TriangleHeight = Convert.ToDouble(Console.ReadLine());
-                double AreaOfTriangle = (TriangleBase * TriangleHeight) / 2;
-                Console.WriteLine("Area of triangle is: " + AreaOfTriangle);
+                double triangleHeight = Convert.ToDouble(Console.ReadLine());
 
-                // Tiles needed 
-                int TilesNeeded = (int)Math.Ceiling(AreaOfTriangle / SizeofTile); // Rounded up
-                Console.WriteLine("The total tiles needed is: " + TilesNeeded + ".");
-
-                // Calculating the Total Cost
-                double total = TilesNeeded * CostofTile;
-                Console.WriteLine("The Total Cost is: $" + total + ".");
-
-                //since work done on 20 square feet is 
-                //#86/hr. Then, I square feet costs 86/20 = $4.3
-                double labourCost = AreaOfTriangle * (86 / 20);
-                Console.WriteLine("The labor cost is: $" + labourCost);
+                // Calculate the are of the Triangle
+                area = (triangleBase * triangleHeight) / 2;
+                Console.WriteLine("Area of triangle is: " + area);
             }
             else
             {
                 Console.WriteLine("Shape is invalid");
             }
-            
+
+            // Tiles needed 
+            tilesNeeded = (int)Math.Ceiling(area / sizeofTile); // Rounded up
+            Console.WriteLine("The total tiles needed is: " + tilesNeeded + ".");
+
+            // Calculating the Total Cost
+            total = tilesNeeded * costofTile;
+            Console.WriteLine("The Total Cost is: $" + total + ".");
+
+            //since work done on 20 square feet is 
+            //#86/hr. Then:
+            double workdone = 86 / 20;
+            labourCost = area * workdone;
+            Console.WriteLine("The labor cost is: $" + labourCost);
         }
 
     }
 }
-
-

7980f1edc3baee606189fc3335f933868bff004a
Olayode Yusuff <getlayus@gmail.com>
2023-08-15 20:12:45 +0100

    Fix Issue #6 Empty lines removedFix

--- TileCostCalculator/TileCostCalculator/Program.cs
+++ TileCostCalculator/TileCostCalculator/Program.cs
@@ -1,6 +1,4 @@
 using System;
-
-
 namespace TileCostCalculator
 {
     internal class Program
@@ -10,37 +8,26 @@ namespace TileCostCalculator
             // Supply the shape of the room
             Console.WriteLine("Please select whether the shape of the room. For rectangular, enter r. For Triangle enter t.");
             string shapeOfRoom = Console.ReadLine().ToUpper();
-
-
-
             // Supply the Cost/unit of Tile
             Console.WriteLine("Please Supply the Cost of Tile/Square Feet in $ : ");
             double costOfTile = Convert.ToDouble(Console.ReadLine());
 
-            // Supply the size of each unit of tiles in square feet
+            // Supply the size of each unit of tiles in square feet.
             Console.WriteLine("Size of each unit of tiles in square feet : ");
             double sizeOfTile = Convert.ToDouble(Console.ReadLine());
-
-
-
             double area = 0;
-
             // Calculating the Area to floor 
             if (shapeOfRoom == "R")
-
             {
                 //Supply the lenght
                 Console.WriteLine("Good day. Please Supply the flooring Length in feet");
                 double lengthOfRoom = Convert.ToDouble(Console.ReadLine());
-
                 // Supply the Width
                 Console.WriteLine("Please Supply the flooring width in feet");
                 double widthOfRoom = Convert.ToDouble(Console.ReadLine());
-
                 // Calculating the Area to floor 
                 area = lengthOfRoom * widthOfRoom;
                 Console.WriteLine("Area of Rectangle is: " + area);
-
             }
             else if (shapeOfRoom == "T")
             {
@@ -60,16 +47,12 @@ namespace TileCostCalculator
             {
                 Console.WriteLine("Shape is invalid");
             }
-
             // Tiles needed 
             double tilesNeeded = (int)Math.Ceiling(area / sizeOfTile); // Rounded up
             Console.WriteLine("The total tiles needed is: " + tilesNeeded + ".");
-
             // Calculating the Total Cost
             double total = tilesNeeded * costOfTile;
             Console.WriteLine("The Total Cost is: $" + total + ".");
-
-
             // since 20 Square Feet is done every hour at the rate of $86/hr. Then:
             //1 Sqaure Feet is done at 86/hr divide 20. 
             double givenFeet = 20;

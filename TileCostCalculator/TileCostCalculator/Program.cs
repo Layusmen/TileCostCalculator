5150766b1184bf36aa5f8e5f26916d8d225b6db5
Olayode Yusuff <getlayus@gmail.com>
2023-08-18 10:11:58 +0100

    Fix #11 Unnecessary brackets removed

--- TileCostCalculator/TileCostCalculator/Program.cs
+++ TileCostCalculator/TileCostCalculator/Program.cs
@@ -34,37 +34,36 @@ namespace TileCostCalculator
             double sizeOfTile = Convert.ToDouble(Console.ReadLine());
             double area = 0;
 
+            //Calculate the Area to floor 
+            if (shapeOfRoom == "R")
             {
+                //Supply the lenght
+                Console.WriteLine("Good day. Please Supply the flooring Length in feet");
+                double lengthOfRoom = Convert.ToDouble(Console.ReadLine());
+
+                //Supply the Width
+                Console.WriteLine("Please Supply the flooring width in feet");
+                double widthOfRoom = Convert.ToDouble(Console.ReadLine());
+
                 //Calculate the Area to floor 
-                if (shapeOfRoom == "R")
-                {
-                    //Supply the lenght
-                    Console.WriteLine("Good day. Please Supply the flooring Length in feet");
-                    double lengthOfRoom = Convert.ToDouble(Console.ReadLine());
-
-                    //Supply the Width
-                    Console.WriteLine("Please Supply the flooring width in feet");
-                    double widthOfRoom = Convert.ToDouble(Console.ReadLine());
-
-                    //Calculate the Area to floor 
-                    area = lengthOfRoom * widthOfRoom;
-                    Console.WriteLine("Area of Rectangle is: " + area);
-                }
-                else if (shapeOfRoom == "T")
-                {
-                    //Supply the base
-                    Console.WriteLine("Enter the base of triangle:");
-                    double triangleBase = Convert.ToDouble(Console.ReadLine());
-
-                    //Supply the height
-                    Console.WriteLine("Enter the height of triangle:");
-                    double triangleHeight = Convert.ToDouble(Console.ReadLine());
-
-                    //Calculate the are of the Triangle
-                    area = (triangleBase * triangleHeight) / 2;
-                    Console.WriteLine("Area of triangle is: " + area);
-                }
+                area = lengthOfRoom * widthOfRoom;
+                Console.WriteLine("Area of Rectangle is: " + area);
             }
+            else if (shapeOfRoom == "T")
+            {
+                //Supply the base
+                Console.WriteLine("Enter the base of triangle:");
+                double triangleBase = Convert.ToDouble(Console.ReadLine());
+
+                //Supply the height
+                Console.WriteLine("Enter the height of triangle:");
+                double triangleHeight = Convert.ToDouble(Console.ReadLine());
+
+                //Calculate the are of the Triangle
+                area = (triangleBase * triangleHeight) / 2;
+                Console.WriteLine("Area of triangle is: " + area);
+            }
+
             string dollar = "$";
 
             //Tiles needed 
